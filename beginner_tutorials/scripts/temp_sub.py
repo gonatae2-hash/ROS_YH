f72ec9e1c2d3bfd08516c79e47a666eacf748d98
gonatae2 <gonatae2@gmail.com>
2026-03-03 23:51:18 -0800

    경고 문구 추가

--- beginner_tutorials/scripts/temp_sub.py
+++ beginner_tutorials/scripts/temp_sub.py
@@ -7,6 +7,8 @@ from std_msgs.msg import Float32
 def callback(msg):
 
     rospy.loginfo("온도: %0.1f", msg.data)
+    if msg.data > 35.0:
+        rospy.loginfo("온도가 너무 높습니다 : %0.1f", msg.data)
 
 def listener():
 
